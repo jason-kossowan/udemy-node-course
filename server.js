fe367f0e68829acd43c65ebbaeca4ea39fbf0b62
Jason Kossowan <jason.kossowan@zedisolutions.com>
2017-08-04 13:17:53 -0600

    Removed maintenance shim

--- server.js
+++ server.js
@@ -22,9 +22,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    //res.render('maintenance.hbs');
-});
+// uncomment to post a maintenance page
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(`${__dirname}/public`));
 
